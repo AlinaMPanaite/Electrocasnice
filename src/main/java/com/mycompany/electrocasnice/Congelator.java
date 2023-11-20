a8cffa7ceaf800e2867a0520cd32d40fda6ea2e3
Alina-Magdalena Panaite <150840130+AlinaMPanaite@users.noreply.github.com>
2023-11-20 20:14:14 +0200

    Update Congelator.java

--- src/main/java/com/mycompany/electrocasnice/Congelator.java
+++ src/main/java/com/mycompany/electrocasnice/Congelator.java
@@ -1,53 +1,48 @@
-package electrocasnice;
+package com.mycompany.electrocasnice;
 
 public class Congelator extends Electrocasnice {
-   private String clasaCongelator; // Parametrul specific clasei Frigider
+
     String sistemDeRacire;
     int numarCompresoare;
     String controlTemperatura;
     int nrRafturi;
-    boolean dozatorApa;
     boolean cutieFructesiLegume;
     int nivelZgomot;
-    
- public Congelator() {
-        super(); // Apelează constructorul implicit din clasa de bază Electrocasnice
-        clasaCongelator = " "; // Clasa frigiderului
+
+    public Congelator() {
+        super(); 
         sistemDeRacire = " ";
         numarCompresoare = 0;
         controlTemperatura = " ";
         nrRafturi = 0;
-        dozatorApa = false;
         cutieFructesiLegume = false;
         nivelZgomot = 0;
     }
-    // Constructor pentru subclasa Frigider
-    Congelator(String clasaCongelator, String sistemDeRacire, int numarCompresoare, String controlTemperatura, int nrRafturi, boolean dozatorApa, boolean cutieFructesiLegume, int nivelZgomot, String brand, String tip, String clasaEficientaEnergetic, String numeModel, String culoare, String functii, float capacitate, int recenziiTotal, float pret, float recenzii, String dimensiune, float greutate, int garantiePersoaneJuridice, int garantiePersoaneFizice) {
-    super(tip, brand, numeModel, functii, capacitate, clasaEficientaEnergetic, pret, greutate, dimensiune, culoare, recenzii, recenziiTotal, garantiePersoaneJuridice, garantiePersoaneFizice);
-        this.clasaCongelator = clasaCongelator;
+
+    Congelator(String sistemDeRacire, int numarCompresoare, String controlTemperatura, int nrRafturi, boolean cutieFructesiLegume, int nivelZgomot, String brand, String tip, String clasaEficientaEnergetica, String numeModel, String culoare, String functii, float capacitate, int recenziiTotal, float pret, float recenzii, String dimensiune, float greutate, int garantiePersoaneJuridice, int garantiePersoaneFizice) {
+        super(tip, brand, numeModel, functii, capacitate, clasaEficientaEnergetica, pret, greutate, dimensiune, culoare, recenzii, recenziiTotal, garantiePersoaneJuridice, garantiePersoaneFizice);
+        // this.clasaCongelator = clasaCongelator;
         this.sistemDeRacire = sistemDeRacire;
         this.numarCompresoare = numarCompresoare;
         this.controlTemperatura = controlTemperatura;
         this.nrRafturi = nrRafturi;
-        this.dozatorApa = dozatorApa;
+        // this.dozatorApa = dozatorApa;
         this.cutieFructesiLegume = cutieFructesiLegume;
         this.nivelZgomot = nivelZgomot;
     }
-        
+
     public Congelator(Congelator copie) {
-        super(copie); // Apelează constructorul de copiere din clasa de bază
-        this.clasaCongelator = copie.clasaCongelator;
+        super(copie); 
         this.sistemDeRacire = copie.sistemDeRacire;
         this.numarCompresoare = copie.numarCompresoare;
         this.controlTemperatura = copie.controlTemperatura;
         this.nrRafturi = copie.nrRafturi;
-        this.dozatorApa = copie.dozatorApa;
         this.cutieFructesiLegume = copie.cutieFructesiLegume;
         this.nivelZgomot = copie.nivelZgomot;
     }
-    @Override
-     public String toString() {
-        return super.toString() + " Clasa Congelator: " + clasaCongelator + ", Sistem de racire: " + sistemDeRacire  + ", Numar compresoare: " + numarCompresoare + ", Control temperatura: " + controlTemperatura + ", Numar de rafturi: " + nrRafturi + ", Dozator de apa: " + dozatorApa + ", Cutie de Fructe si Legume: " + cutieFructesiLegume + ", Nivel Zgomot: " + nivelZgomot + "dB";
-    }
 
+    @Override
+    public String toString() {
+        return super.toString() + " \n  Tip:" + tip + " \n  Sistem de Racire:" + sistemDeRacire + "\n  Numar compresoare: " + numarCompresoare + " \n  Control Temperatura: " + controlTemperatura + " \n  Numar de rafturi: " + nrRafturi + " \n  Cutie Fructe si Legume: " + cutieFructesiLegume + " \n \n  Caracteristici tehnice " + " \n  Clasa Eficienta energetica: " + clasaEficientaEnergetica + " \n  Nivel zgomot(dB): " + nivelZgomot + " \n \n  Garantii" + " \n  Garantie comerciala persoane juridice: " + garantiePersoaneJuridice + " luni" + " \n  Garantie de conformitate persoane fizice: " + garantiePersoaneFizice + " luni";
+    }
 }
